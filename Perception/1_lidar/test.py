d27f6c6ff8f67929292c0b8382ca22e9ccf6dd77
J1dan <513037912@qq.com>
2023-01-27 18:39:58 +0800

    fix: recovered intensity

--- Perception/1_lidar/test.py
+++ Perception/1_lidar/test.py
@@ -2,13 +2,8 @@ import numpy as np
 
 arr = np.arange(12).reshape([6,2])
 index = [True, True, False, False, False, False]
-# replace = np.ones([1,6])
-x = arr[index]
-print(f"x = {x}")
-print(f"arr[index] = {arr[index]}")
-x[:,1] = 100
-arr[index] = x
-print(f"x = {x}")
-print(f"arr[index] = {arr[index]}")
+replace = np.ones([1,6])
+arr = np.insert(arr,2,replace,axis=1)
+print(arr)
 
 
